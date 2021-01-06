fe4855c3abbb8724bac8ea406e8bbea772d242df
Rio Arjuna <ri0arjuna@programmer.net>
2021-01-06 19:59:42 +0700

    - add parcelable implementation to passing as object with intent or bundle

--- data/src/main/java/rio/arj/infootball/data/repository/nextleague/model/Events.kt
+++ data/src/main/java/rio/arj/infootball/data/repository/nextleague/model/Events.kt
@@ -1,8 +1,11 @@
 package rio.arj.infootball.data.repository.nextleague.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 import rio.arj.core.utils.DateUtils
 
+@Parcelize
 data class Events(
   @SerializedName("idEvent")
   val idEvent: String,
@@ -16,7 +19,7 @@ data class Events(
   val leagueName: String,
   @SerializedName("strVenue")
   val venueName: String,
-) {
+) : Parcelable {
   fun getDateFormatted(): String {
     return DateUtils().convertSimpleDateFormat(dateEvent)
   }
